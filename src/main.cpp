66747db49b8ced1668b2eaa975ac026b72c4700f
Jon Hylands <jhylands@mozilla.com>
2013-12-30 21:40:22 -0500

    moved pin and other constants to main.h

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,7 @@
 #include "RangeFinder.h"
 #include "HeartbeatLED.h"
 #include "MotorDriver.h"
-
-// Teensy 3.1 has the LED on pin 13
-const int ledPin = 13;
-const int serialBaudRate = 115200;
-
-const int sharpPin = 21;
-const int proxDotPin = 20;
-
-const int rightEnablePin = 7;
-const int rightDirectionPin = 8;
-const int rightPwmPin = 9;
-const int leftPwmPin = 10;
-const int leftDirectionPin = 11;
-const int leftEnablePin = 12;
-
-const int leftCurrentPin = 15;
-const int rightCurrentPin = 14;
+#include "main.h"
 
 RangeFinder rangeFinder;
 HeartbeatLED heartbeat;
@@ -32,9 +16,8 @@ MotorDriver leftMotor;
 MotorDriver rightMotor;
 
 void setup() {
-	// initialize the digital pin as an output.
 	Serial1.begin(serialBaudRate);
-	Serial1.println("uCee Range Test");
+	Serial1.println("uCee Test");
 	rangeFinder.begin(sharpPin, proxDotPin);
 	heartbeat.begin(ledPin, 100, 900);
 	leftMotor.begin(leftPwmPin, leftDirectionPin, leftEnablePin, leftCurrentPin);
---
+++ src/main.h
@@ -0,0 +1,20 @@
+#ifndef Main_h
+#define Main_h
+
+const int ledPin = 13;
+const int serialBaudRate = 115200;
+
+const int sharpPin = 21;
+const int proxDotPin = 20;
+
+const int rightEnablePin = 7;
+const int rightDirectionPin = 8;
+const int rightPwmPin = 9;
+const int leftPwmPin = 10;
+const int leftDirectionPin = 11;
+const int leftEnablePin = 12;
+
+const int leftCurrentPin = 15;
+const int rightCurrentPin = 14;
+
+#endif // Main_h
